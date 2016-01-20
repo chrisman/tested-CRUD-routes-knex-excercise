a88ddc6b1e7d620730226dce3ca417597fb52281
Christopher Brown <christopher.p.brown@gmail.com>
2016-01-19 20:43:10 -0700

    404

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/movies', reviews);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Sorry, can\'t find that!');
   err.status = 404;
   next(err);
 });
